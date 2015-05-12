ae05e6438796f3813fe1f24992cc0a4172576397
fabian rios <fabianrios@iMac-de-fabian.local>
2015-05-12 12:31:02 -0500

    artist individual

--- app/scripts/controllers/artista.js
+++ app/scripts/controllers/artista.js
@@ -7,9 +7,22 @@
  * # ArtistaCtrl
  * Controller of the lastFmApp
  */
+
+var BASE_URL = "http://ws.audioscrobbler.com/2.0/?api_key=6c99bbdb7918ba66256380a94e702da3&format=json";
+
 angular.module('lastFmApp')
-  .controller('ArtistaCtrl', function ($scope, $routeParams) {
+  .factory('artistGet', function($resource){
+    return $resource(BASE_URL + '&method=artist.getinfo&mbid=:mbid', {}, {
+          query: {method:'GET', isArray:false}
+    });
+  })
+  .controller('ArtistaCtrl', function ($scope, $routeParams, artistGet) {
     
-    console.log($routeParams.id);
+    
+    var res = artistGet.query({mbid: $routeParams.id}, function () {
+      $scope.artist = res.artist;
+      console.log($scope.artist);
+      // $scope.artists = res.results.artistmatches.artist;
+    });
     
   });
